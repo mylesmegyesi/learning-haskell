235b36a0f2714a217831f8ce964e5818321d3496
Myles Megyesi <mylesmegyesi@me.com>
2014-09-26 07:32:02 -0500

    remove solveRPN build

--- src/LearnYouAHaskell/SolveRPN.hs
+++ src/LearnYouAHaskell/SolveRPN.hs
@@ -1,14 +1,8 @@
 module LearnYouAHaskell.SolveRPN (solveRPN) where
 
-import System.Environment (getArgs)
-
 solveRPN :: (Num a, Read a) => String -> a
 solveRPN = head . foldl evalString [] . words
   where evalString (x:y:ys) "-"  = (y-x):ys
         evalString (x:y:ys) "+"  = (x+y):ys
         evalString (x:y:ys) "*"  = (x*y):ys
         evalString stack    item = read item:stack
-
-main = do
-  (expression:args) <- getArgs
-  print $ solveRPN expression
