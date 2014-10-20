b361d9722e0d153b9b796e6b7803a701f454dbc1
Myles Megyesi <mylesmegyesi@me.com>
2014-10-20 08:12:37 -0500

    lisp reader now parses ints, strings and lists

--- src/Lisp/Reader.hs
+++ src/Lisp/Reader.hs
@@ -1,24 +1,75 @@
-module Lisp.Reader (
-  read
-  , ASTNode(..)
-  ) where
+{-# LANGUAGE FlexibleContexts #-}
 
-import Prelude hiding (read)
-import Data.Char (isDigit)
+module Lisp.Reader
+    ( ASTNode(IntegerNode, StringNode, ListNode)
+    , readString
+    ) where
 
-data ASTNode = IntegerNode String | StringNode String deriving (Show, Eq)
+import Control.Applicative
+     ( (<|>)
+     )
 
-readInt :: String -> ASTNode
-readInt [] = (IntegerNode [])
-readInt (first:rest)
-    | (isDigit first) || (first == '-') = IntegerNode (first:restAsInt)
-    | otherwise = IntegerNode []
-  where (IntegerNode restAsInt) = readInt rest
+import Text.ParserCombinators.Parsec
+    ( Parser
+    , char
+    , digit
+    , many
+    , many1
+    , noneOf
+    , parse
+    , optionMaybe
+    , sepBy
+    , spaces
+    )
 
-read :: String -> ASTNode
-read [] = (IntegerNode "")
-read arg@(first:rest)
-    | (first == ' ') = read rest
-    | (first == '"') = (StringNode (init rest))
-    | (isDigit first) || (first == '-') = readInt arg
-    | otherwise = read []
+import Text.Parsec.Prim
+    ( Stream
+    , ParsecT
+    )
+
+data ASTNode = IntegerNode Bool String
+             | StringNode String
+             | ListNode [ASTNode]
+             deriving (Show, Eq)
+
+parseInt :: Parser ASTNode
+parseInt = do isNegative <- optionMaybe (char '-')
+              result <- many1 digit
+              case isNegative of
+                  Just _ -> return (IntegerNode False result)
+                  Nothing -> return (IntegerNode True result)
+
+parseString :: Parser ASTNode
+parseString = do _ <- char '"'
+                 str <- many $ noneOf ['"']
+                 _ <- char '"'
+                 return (StringNode str)
+
+elementSeparator :: Stream s m Char => ParsecT s u m ()
+elementSeparator = do spaces
+                      _ <- optionMaybe (char ',')
+                      spaces
+                      return ()
+
+parseList :: Parser ASTNode
+parseList = do _ <- char '('
+               exprs <- (parseExpr `sepBy` elementSeparator)
+               _ <- char ')'
+               return (ListNode exprs)
+
+parseExpr :: Parser ASTNode
+parseExpr = do spaces
+               result <- parseInt <|> parseString <|> parseList
+               spaces
+               return result
+
+lispParser :: Parser [ASTNode]
+lispParser = do results <- many1 parseExpr
+                spaces
+                return results
+
+readString :: String -> [ASTNode]
+readString s =
+    case parse lispParser "" s of
+        Left err -> error $ (show err)
+        Right exprs -> exprs
--- test/Lisp/ReaderSpec.hs
+++ test/Lisp/ReaderSpec.hs
@@ -1,27 +1,91 @@
 module Lisp.ReaderSpec where
 
-import Test.Hspec (Spec, describe, it, shouldBe)
-import Lisp.Reader (read, ASTNode(IntegerNode, StringNode))
-import Prelude hiding (read)
+import Test.Hspec (Spec, describe, it, shouldBe, pendingWith)
+import Lisp.Reader
+    (
+      ASTNode(IntegerNode, StringNode, ListNode)
+    , readString
+    )
 
 spec :: Spec
 spec =
   describe "Lisp.Reader" $ do
-    it "reads an integer" $ do
-      read "1" `shouldBe` (IntegerNode "1")
-      read "2" `shouldBe` (IntegerNode "2")
-      read "0" `shouldBe` (IntegerNode "0")
-      read "-1" `shouldBe` (IntegerNode "-1")
-      read "22" `shouldBe` (IntegerNode "22")
-      read "-22" `shouldBe` (IntegerNode "-22")
-      read "-22" `shouldBe` (IntegerNode "-22")
+    describe "reading integers" $ do
+      it "reads a single positive digit" $ do
+        readString "1" `shouldBe` [(IntegerNode True "1")]
+        readString "2" `shouldBe` [(IntegerNode True "2")]
+        readString "0" `shouldBe` [(IntegerNode True "0")]
 
-    it "reads a string" $ do
-      read "\"howdy\"" `shouldBe` (StringNode "howdy")
+      it "reads multiple positive digits" $ do
+        readString "22" `shouldBe` [(IntegerNode True "22")]
 
-    it "ignores leading whitespace" $ do
-      read " 1" `shouldBe` (IntegerNode "1")
+      it "reads a single negative integer" $ do
+        readString "-1" `shouldBe` [(IntegerNode False "1")]
 
-    it "ignores trailing whitespace" $ do
-      read "1 " `shouldBe` (IntegerNode "1")
-      read "-22 " `shouldBe` (IntegerNode "-22")
+      it "reads multiple negative integer" $ do
+        readString "-22" `shouldBe` [(IntegerNode False "22")]
+
+      it "ignores leading whitespace" $ do
+        readString " 1" `shouldBe` [(IntegerNode True "1")]
+
+      it "ignores trailing whitespace of a positive number" $ do
+        readString "22 " `shouldBe` [(IntegerNode True "22")]
+
+      it "ignores trailing whitespace of a negative number" $ do
+        readString "-22 " `shouldBe` [(IntegerNode False "22")]
+
+    describe "reading strings" $ do
+      it "reads a string" $ do
+        readString "\"howdy\"" `shouldBe` [(StringNode "howdy")]
+
+      it "ignores leading whitespace" $ do
+        readString " \"howdy\"" `shouldBe` [(StringNode "howdy")]
+
+      it "ignores trailing whitespace" $ do
+        readString "\"howdy\" " `shouldBe` [(StringNode "howdy")]
+
+      it "does not allow quotes in quotes" $
+        pendingWith "test error case"
+
+    describe "reading lists" $ do
+      it "reads an empty list" $ do
+        readString "()" `shouldBe` [(ListNode [])]
+
+      it "ignores leading spaces" $ do
+        readString " ()" `shouldBe` [(ListNode [])]
+
+      it "ignores trailing spaces" $ do
+        readString "  ()  " `shouldBe` [(ListNode [])]
+
+      it "reads a list with one integer" $ do
+        readString "(1)" `shouldBe` [(ListNode [(IntegerNode True "1")])]
+
+      it "ignores spaces leading the first value" $ do
+        readString "(  1) " `shouldBe` [(ListNode [(IntegerNode True "1")])]
+
+      it "ignores spaces trailing the first value" $ do
+        readString "(   1   ) " `shouldBe` [(ListNode [(IntegerNode True "1")])]
+
+      it "reads a list with two integers" $ do
+        readString "(1 2)" `shouldBe` [(ListNode [ (IntegerNode True "1")
+                                                 , (IntegerNode True "2")
+                                                 ])]
+
+      it "ignores spaces trailing the first value" $ do
+        readString "( 1   2   ) " `shouldBe` [(ListNode [ (IntegerNode True "1")
+                                                        , (IntegerNode True "2")
+                                                        ])]
+
+      it "handles commas between elements of the list" $ do
+        readString "(1, 2)" `shouldBe` [(ListNode [ (IntegerNode True "1")
+                                                  , (IntegerNode True "2")
+                                                  ])]
+
+      it "does not allow leading commas before elements in a list" $ do
+        pendingWith "readString \"(,1)\""
+
+      it "does not allow a trailing commas after the last element in a list" $ do
+        pendingWith "readString \"(1,)\""
+
+      it "does not allow multiple commas between elements in a list" $ do
+        pendingWith "readString \"(1 ,, 1)\""
